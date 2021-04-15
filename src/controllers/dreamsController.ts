dc2d52a57e61bf5bb73b4ffc5b1b011e069bcef6
Kristina <tina91kris11@gmail.com>
2021-04-15 17:30:10 +0200

    Getting all dreams route - done.

--- src/controllers/dreamsController.ts
+++ src/controllers/dreamsController.ts
@@ -3,7 +3,9 @@ const Dream = require('../models/Dream');
 
 // Methods for /dreams Get, Post and Delete
 export const dreams_getAll = (req: Request, res: Response) => {
-  res.json('Getting all dreams');
+  Dream.find({})
+    .then((data: unknown) => res.status(200).send(data))
+    .catch((err: unknown) => res.status(500).send(err))
 }
 
 // Post a dream to a db
@@ -18,27 +20,31 @@ export const dreams_postAll = (req: Request, res: Response) => {
   // Saving newDream to a db
   newDream.save()
     .then(() => {
-      res.send('Succsessfully created a new dream :)');
+      res.status(200).send('Succsessfully created a new dream :)');
     })
-    .catch((err: unknown) => res.send(err));
+    .catch((err: unknown) => res.status(500).send(err));
 }
 
 // Deleting all dreams
 export const dreams_deleteAll = (req: Request, res: Response) => {
   Dream.deleteMany({})
-    .then(() => res.send('Deleted All Dreams'))
-    .catch((err: unknown) => res.send(err));
+    .then(() => res.status(200).send('Deleted All Dreams'))
+    .catch((err: unknown) => res.status(500).send(err));
 }
 
 // Methods for specific dream
+// Getting one dream
 export const dreams_get = (req: Request, res: Response) => {
   res.json('Getting one dream');
 }
 
+
+// Updating one dream
 export const dreams_patch = (req: Request, res: Response) => {
   res.json('Updating one dream');
 }
 
+// Deleting one dream
 export const dreams_delete = (req: Request, res: Response) => {
   res.json('Deleting one dream');
 }
